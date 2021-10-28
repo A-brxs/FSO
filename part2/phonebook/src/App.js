ae6f1ca6e28b251ecc1094f6e5c5165607f8f08a
luis zuniga <lzuniga@smvn.io>
2021-10-28 23:26:33 +0300

    Exercise 2.11 OK

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Search = (props) => {
   const {newSearch, handleSearch} = props
@@ -39,12 +40,7 @@ const Filter = ({persons,search}) => {
 
 const App = () => {
   console.log('const App loaded')
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('') 
   const [ newNumber, setNewNumber ] = useState('') 
   const [ newSearch, setSearch ] = useState('') 
@@ -86,6 +82,18 @@ const App = () => {
   
   const checkPerson = () => persons.find( who => who.name === newName && who.number === newNumber )
   
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
+
+
   return (
     <div>
       <h2>Phonebook</h2>
